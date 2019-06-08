39cd0bc323fad3a3c70953580a10e8603da434f1
react7press <react7.store@gmail.com>
2019-06-08 17:04:07 +0800

    Initial commit

---
+++ Backend/admin/config.php
@@ -0,0 +1,35 @@
+<?php
+
+
+
+/* URL PROJECT */
+
+define ('SITE_URL', 'http://localhost:8888/babelApp/backend');
+
+/* DATABASE CONFIGURATION */
+
+$database = array(
+'host' => 'localhost:8888',
+'db' => 'babel',
+'user' => 'babeluser',
+'pass' => '121073ms'
+);
+
+$email_config = array(
+'email_address' => 'ed@react7.press',
+'email_password' => '121073ms',
+'email_subject' => 'EMAIL_SUBJECT_HERE',
+'email_name' => 'EMAIL_NAME_HERE',
+'smtp_host' => 'EMAIL_HOST_HERE',
+'smtp_port' => 'EMAIL_PORT_HERE',
+'smtp_encrypt' => 'tls'
+);
+
+$items_config = array(
+
+    'items_per_page' => '8',
+    'images_folder' => 'images/'
+);
+
+
+?>
